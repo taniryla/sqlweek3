533c4d56f09d582ff3bdbc89dd4774776e0a198a
Chako <ryland0000@gmail.com>
2023-09-08 20:26:07 -0700

    raw SQL code

--- psycopg2/veggies.py
+++ psycopg2/veggies.py
@@ -3,6 +3,51 @@ import psycopg2
 # connect to your postgres db
 conn = psycopg2.connect(
     """
-    dbname=week 3 user=postgres host=localhost port=5432
+    dbname=week3 user=postgres host=localhost port=5432
     """
 )
+conn.set_session(autocommit=True)
+
+# open a cursor to perform database operations
+cur = conn.cursor()
+
+cur.execute(
+    """
+    DROP TABLE IF EXISTS veggies
+    """
+)
+
+cur.execute(
+    """
+    CREATE TABLE veggies(
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        color TEXT NOT NULL
+    )
+    """
+)
+
+cur.execute(
+    """
+    INSERT INTO veggies VALUES 
+    (1, 'carrot', 'orange'),
+    (2, 'onion', 'yellow'),
+    (3, 'zucchini', 'green'),
+    (4, 'squash', 'yellow'),
+    (5, 'pepper', 'red'),
+    (6, 'onion', 'red')
+    """
+)
+
+# execute a query
+cur.execute(
+    """
+    SELECT * FROM veggies
+    """
+)
+
+
+# retrieve query results
+records = cur.fetchall()
+
+print(records)
