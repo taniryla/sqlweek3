9e0091a0817b37939a3bf33b4051922a3422eb5f
Chako <ryland0000@gmail.com>
2023-09-08 21:45:07 -0700

    seed the data

--- sqlalchemy/veggies.py
+++ sqlalchemy/veggies.py
@@ -13,3 +13,28 @@ class Veggie(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     color = Column(String, nullable=False)
     name = Column(String, nullable=False)
+
+
+# Recreate all tables each time script is run
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+seed_data = [
+    {'name': 'carrot', 'color': 'orange'},
+    {'name': 'onion', 'color': 'yellow'},
+    {'name': 'zucchini', 'color': 'green'},
+    {'name': 'squash', 'color': 'yellow'},
+    {'name': 'pepper', 'color': 'red'},
+    {'name': 'onion', 'color': 'red'}
+]
+
+# turn the seed data into a list of veggie objects
+veggie_objects = []
+for item in seed_data:
+    v = Veggie(name=item["name"], color=item["color"])
+    veggie_objects.append(v)
+
+# create a session, insert new records, and commit the session
+session = Session()
+session.bulk_save_objects(veggie_objects)
+session.commit()
