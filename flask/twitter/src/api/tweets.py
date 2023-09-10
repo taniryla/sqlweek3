e92098b5d36eb9d8eb4916cb84f2c9377692f3aa
Chako <ryland0000@gmail.com>
2023-09-10 02:29:13 -0700

    route parameter

--- flask/twitter/src/api/tweets.py
+++ flask/twitter/src/api/tweets.py
@@ -3,10 +3,15 @@ from ..models import Tweet, User, db
 
 bp = Blueprint('tweets', __name__, url_prefix='/tweets')
 
-@bp.route('', methods=['GET']) #DECORATOR TAKES PATH AND LIST OF HTT VERBS
+@bp.route('', methods=['GET']) #DECORATOR TAKES PATH AND LIST OF HTTP VERBS
 def index():
     tweets = Tweet.query.all() # ORM performs SELECT query
     result = []
     for t in tweets:
         result.append(t.serialize()) # build list of Tweets as dictionaries
     return jsonify(result) # return JSON response
+
+@bp.route('/<int:id>', methods=['GET'])
+def show(id: int):
+    t = Tweet.query.get_or_404(id, "Tweet not found")
+    return jsonify(t.serialize())
