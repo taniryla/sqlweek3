126ac509590cb7411d3254881175e9615041f33d
Chako <ryland0000@gmail.com>
2023-09-10 02:24:46 -0700

    decorator route

--- flask/twitter/src/api/tweets.py
+++ flask/twitter/src/api/tweets.py
@@ -1,3 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, request
+from ..models import Tweet, User, db
 
 bp = Blueprint('tweets', __name__, url_prefix='/tweets')
+
+@bp.route('', methods=['GET']) #DECORATOR TAKES PATH AND LIST OF HTT VERBS
+def index():
+    tweets = Tweet.query.all() # ORM performs SELECT query
+    result = []
+    for t in tweets:
+        result.append(t.serialize()) # build list of Tweets as dictionaries
+    return jsonify(result) # return JSON response
