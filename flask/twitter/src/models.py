b98a6d79f84b58ccbfb9a60301269b78dd5c744e
Chako <ryland0000@gmail.com>
2023-09-10 01:07:43 -0700

    liking_users

--- flask/twitter/src/models.py
+++ flask/twitter/src/models.py
@@ -8,6 +8,26 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    tweets = db.relationship('Tweet', backref='user', cascade="all,delete")
+
+likes_table = db.Table(
+    'likes',
+    db.Column(
+        'user_id', db.Integer,
+        db.ForeignKey('users.id'),
+        primary_key=True
+    ),
+    db.Column(
+        'tweet_id', db.Integer,
+        db.ForeignKey('tweets.id'),
+        primary_key=True
+    ),
+    db.Column(
+        'created_at', db.DateTime,
+        default=datetime.datetime.utcnow,
+        nullable=False
+    )
+)
 
 class Tweet(db.Model):
     __tablename__ = 'tweets'
@@ -19,3 +39,9 @@ class Tweet(db.Model):
         nullable=False
     )
     user_id = db.Column(db.Integer, db.ForeignKey('users.id', nullable=False))
+
+    liking_users = db.relationship(
+        'User', secondary=likes_table,
+        lazy='subquery',
+        backref=db.backref('liked_tweets', lazy=True)
+    )
