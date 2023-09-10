d5e241dea86dbaebc020a6dac211080fe2aa3384
Chako <ryland0000@gmail.com>
2023-09-10 03:06:21 -0700

    post method

--- flask/twitter/src/api/tweets.py
+++ flask/twitter/src/api/tweets.py
@@ -15,3 +15,20 @@ def index():
 def show(id: int):
     t = Tweet.query.get_or_404(id, "Tweet not found")
     return jsonify(t.serialize())
+
+@bp.rout('', methods=['POST'])
+def create():
+    #req body must contain user_id and content
+    if 'user_id' not in request.json or 'content' not in request.json:
+        return abort(400)
+    #user with id of user_id must exist
+    User.query.get_or_404(request.json['user_id'], "User not found")
+    # construct Tweet
+    t = Tweet(
+        user_id=request.json['user_id'],
+        content=request.json['content']
+    )
+    db.session.add(t) #prepare CREATE statement
+    db.session.commit() #execute CREATE statement
+    return jsonify(t.serialize())
+
