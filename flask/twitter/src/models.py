e55442c5ea039761c85e1fe8d9483284b316e816
Chako <ryland0000@gmail.com>
2023-09-10 00:55:35 -0700

    tweet class

--- flask/twitter/src/models.py
+++ flask/twitter/src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -7,3 +8,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+
+class Tweet(db.Model):
+    __tablename__ = 'tweets'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(280), nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.datetime.utcnow,
+        nullable=False
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', nullable=False))
