ab7b3772ce208d626a7570b88df5ffefbd2b85c1
Chako <ryland0000@gmail.com>
2023-09-10 17:19:54 -0700

    GET request will return a list of users

--- flask/twitter/src/api/users.py
+++ flask/twitter/src/api/users.py
@@ -1,4 +1,12 @@
 from flask import Blueprint
 from ..models import User, db, Tweet, likes_table
 
-db = Blueprint('users', __name__, url_prefix='/users')
+bp = Blueprint('users', __name__, url_prefix='/users')
+
+@bp.route('', methods=['GET']) #DECORATOR TAKES PATH AND LIST OF HTTP VERBS
+def index():
+    users = User.query.all() # ORM performs SELECT query
+    result = []
+    for u in users:
+        result.append(u.serialize()) # build list of Users as dictionaries
+    return jsonify(result) # return JSON response
