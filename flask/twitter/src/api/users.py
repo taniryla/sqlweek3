482d17a1f25737710ea50204f806f6009866f0fe
Chako <ryland0000@gmail.com>
2023-09-10 18:41:57 -0700

    POST request and password scramble

--- flask/twitter/src/api/users.py
+++ flask/twitter/src/api/users.py
@@ -1,5 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, request
 from ..models import User, db, Tweet, likes_table
+import hashlib
+import secrets
+
+def scramble(password: str):
+    """Hash and salt the given password"""
+    salt = secrets.token_hex(16)
+    return hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
 
 bp = Blueprint('users', __name__, url_prefix='/users')
 
@@ -10,3 +17,24 @@ def index():
     for u in users:
         result.append(u.serialize()) # build list of Users as dictionaries
     return jsonify(result) # return JSON response
+
+@bp.route('/<int:id>', methods=['GET'])
+def show(id: int):
+    u = User.query.get_or_404(id, "User not found")
+    return jsonify(u.serialize())
+
+@bp.route('', methods=['POST'])
+def create():
+    #req body must contain user_id and content
+    if 'username' not in request.json or 'password' not in request.json:
+        return abort(400)
+    if len(request.json['username']) <= 3 or len(request.json['password']) <= 9:
+        return abort(400)
+    # construct User
+    u = User(
+        username=request.json['username'],
+        password=scramble(request.json['password'])
+    )
+    db.session.add(u) #prepare CREATE statement
+    db.session.commit() #execute CREATE statement
+    return jsonify(u.serialize())
