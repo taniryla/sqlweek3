6eafb9a35b122a3f35328affbfd7ed59d90fbc79
Chako <ryland0000@gmail.com>
2023-09-10 20:32:35 -0700

    liking_tweets and liking_users

--- flask/twitter/src/api/users.py
+++ flask/twitter/src/api/users.py
@@ -28,7 +28,7 @@ def create():
     #req body must contain user_id and content
     if 'username' not in request.json or 'password' not in request.json:
         return abort(400)
-    if len(request.json['username']) <= 3 or len(request.json['password']) <= 9:
+    if len(request.json['username']) <= 3 or len(request.json['password']) <= 8:
         return abort(400)
     # construct User
     u = User(
@@ -38,3 +38,43 @@ def create():
     db.session.add(u) #prepare CREATE statement
     db.session.commit() #execute CREATE statement
     return jsonify(u.serialize())
+
+@bp.route('/<int:id>', methods=['DELETE'])
+def delete(id: int):
+    u = User.query.get_or_404(id, "User not found")
+    try:
+        db.session.delete(u)    # prepare DELETE statement
+        db.session.commit()     # execute DELETE statement
+        return jsonify(True)
+    except:
+        # something went wrong :(
+        return jsonify(False)
+    
+@bp.route('/<int:id>', methods=['PATCH', 'PUT'])
+def update(id: int):
+    u = User.query.get_or_404(id) # this code queries the user table to find out if a user record with the requested id exists
+     #req body must contain username only, password only and both username and password
+    if 'username' not in request.json or 'password' not in request.json:
+        return abort(400)
+    if len(request.json['username']) <= 3 or len(request.json['password']) <= 8:
+        return abort(400)
+    # construct User
+    u = User(
+        username=request.json['username'],
+        password=scramble(request.json['password'])
+    )
+    try:
+        db.session.update(u)    # prepare UPDATE statement
+        db.session.commit()     # execute UPDATE statement
+        return jsonify(u.serialize())
+    except:
+        # something went wrong :(
+        return jsonify(False)
+    
+@bp.route('/<int:id>/liking_tweets', methods=['GET'])
+def liking_tweets(id: int):
+    u = User.query.get_or_404(id)
+    result = []
+    for t in u.liking_tweets:
+        result.append(t.serialize())
+    return jsonify(result)
